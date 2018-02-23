39ca79fb9f8324d5070ae7403c173294a359a087
gaobo <gaobo@192.168.179.84>
2018-02-23 17:59:32 +0800

    update

---
+++ src/main/java/spring/ioc/BeanFactory.java
@@ -0,0 +1,190 @@
+package spring.ioc;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanDefinitionStoreException;
+import org.springframework.beans.factory.BeanFactoryUtils;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.core.ResolvableType;
+/**
+ * 
+ * @author gaobo
+ * ************************************************************* 第一条主线 *************************************************************
+ * 		从接口BeanFactory到HierarchicalBeanFactory，再到ConfigurableBeanFactory，是一条主要的BeanFactory设计路线。在这条接口设计中包括了getBaen()
+ * 这样的IOC容器基本方法，而HierarchicalBeanFactory接口在继承了BeanFactory之后增加了getParentBeanFactory()方法，使BeanFactory具有了双亲IOC管理功能，
+ * 在接下来ConfigurableBeanFactory接口中，主要定义一些BeanFactory的配置功能。比如setParentBeanFactory()设置双亲Ioc容器，通过addBeanPostProessor()
+ * 配置Bean后置处理器等等。
+ * 
+ * ************************************************************* 第二条主线 *************************************************************
+ * 		以ApplicationContext应用上下文为核心接口设计。这里主要的接口设计有，从BeanFactory，再到ApplicationContext，再到WebApplicationContext或者
+ *ConfigurableApplicationContext。我们平常应用上下文基本上都是这两个类的实现。ListableBeanFactory和HierarchicalBeanFactory这两个接口，链接BeanFactory
+ *接口定义和ApplicationContext应用上下文的接口定义。在ListableBeanFactory中，细化了很多BeanFactory接口的功能。
+ * 
+ *
+ */
+public interface BeanFactory {
+
+	/**
+	 * 用&得到FactoryBean本身，用来区分通过容器来获取FactoryBean产生的对象和获取FactoryBean本身
+	 */
+	String FACTORY_BEAN_PREFIX = "&";
+
+
+	/**
+	 * 可以获取IoC容器中管理Bean，Bean的获取是通过指定名字来索引的
+	 */
+	Object getBean(String name) throws BeansException;
+
+	/**
+	 *如果需要在获取Bean同时对Bean类型进行检查
+	 */
+	<T> T getBean(String name, Class<T> requiredType) throws BeansException;
+
+	/**
+	 * Return the bean instance that uniquely matches the given object type, if any.
+	 * <p>This method goes into {@link ListableBeanFactory} by-type lookup territory
+	 * but may also be translated into a conventional by-name lookup based on the name
+	 * of the given type. For more extensive retrieval operations across sets of beans,
+	 * use {@link ListableBeanFactory} and/or {@link BeanFactoryUtils}.
+	 * @param requiredType type the bean must match; can be an interface or superclass.
+	 * {@code null} is disallowed.
+	 * @return an instance of the single bean matching the required type
+	 * @throws NoSuchBeanDefinitionException if no bean of the given type was found
+	 * @throws NoUniqueBeanDefinitionException if more than one bean of the given type was found
+	 * @throws BeansException if the bean could not be created
+	 * @since 3.0
+	 * @see ListableBeanFactory
+	 */
+	<T> T getBean(Class<T> requiredType) throws BeansException;
+
+	/**
+	 * Return an instance, which may be shared or independent, of the specified bean.
+	 * <p>Allows for specifying explicit constructor arguments / factory method arguments,
+	 * overriding the specified default arguments (if any) in the bean definition.
+	 * @param name the name of the bean to retrieve
+	 * @param args arguments to use when creating a bean instance using explicit arguments
+	 * (only applied when creating a new instance as opposed to retrieving an existing one)
+	 * @return an instance of the bean
+	 * @throws NoSuchBeanDefinitionException if there is no such bean definition
+	 * @throws BeanDefinitionStoreException if arguments have been given but
+	 * the affected bean isn't a prototype
+	 * @throws BeansException if the bean could not be created
+	 * @since 2.5
+	 */
+	Object getBean(String name, Object... args) throws BeansException;
+
+	/**
+	 * Return an instance, which may be shared or independent, of the specified bean.
+	 * <p>Allows for specifying explicit constructor arguments / factory method arguments,
+	 * overriding the specified default arguments (if any) in the bean definition.
+	 * <p>This method goes into {@link ListableBeanFactory} by-type lookup territory
+	 * but may also be translated into a conventional by-name lookup based on the name
+	 * of the given type. For more extensive retrieval operations across sets of beans,
+	 * use {@link ListableBeanFactory} and/or {@link BeanFactoryUtils}.
+	 * @param requiredType type the bean must match; can be an interface or superclass.
+	 * {@code null} is disallowed.
+	 * @param args arguments to use when creating a bean instance using explicit arguments
+	 * (only applied when creating a new instance as opposed to retrieving an existing one)
+	 * @return an instance of the bean
+	 * @throws NoSuchBeanDefinitionException if there is no such bean definition
+	 * @throws BeanDefinitionStoreException if arguments have been given but
+	 * the affected bean isn't a prototype
+	 * @throws BeansException if the bean could not be created
+	 * @since 4.1
+	 */
+	<T> T getBean(Class<T> requiredType, Object... args) throws BeansException;
+
+
+	/**
+	 * 让用判断容器是都含有指定名字的Bean
+	 */
+	boolean containsBean(String name);
+
+	/**
+	 * 判断是都单利
+	 */
+	boolean isSingleton(String name) throws NoSuchBeanDefinitionException;
+
+	/**
+	 * Is this bean a prototype? That is, will {@link #getBean} always return
+	 * independent instances?
+	 * <p>Note: This method returning {@code false} does not clearly indicate
+	 * a singleton object. It indicates non-independent instances, which may correspond
+	 * to a scoped bean as well. Use the {@link #isSingleton} operation to explicitly
+	 * check for a shared singleton instance.
+	 * <p>Translates aliases back to the corresponding canonical bean name.
+	 * Will ask the parent factory if the bean cannot be found in this factory instance.
+	 * @param name the name of the bean to query
+	 * @return whether this bean will always deliver independent instances
+	 * @throws NoSuchBeanDefinitionException if there is no bean with the given name
+	 * @since 2.0.3
+	 * @see #getBean
+	 * @see #isSingleton
+	 */
+	boolean isPrototype(String name) throws NoSuchBeanDefinitionException;
+
+	/**
+	 * Check whether the bean with the given name matches the specified type.
+	 * More specifically, check whether a {@link #getBean} call for the given name
+	 * would return an object that is assignable to the specified target type.
+	 * <p>Translates aliases back to the corresponding canonical bean name.
+	 * Will ask the parent factory if the bean cannot be found in this factory instance.
+	 * @param name the name of the bean to query
+	 * @param typeToMatch the type to match against (as a {@code ResolvableType})
+	 * @return {@code true} if the bean type matches,
+	 * {@code false} if it doesn't match or cannot be determined yet
+	 * @throws NoSuchBeanDefinitionException if there is no bean with the given name
+	 * @since 4.2
+	 * @see #getBean
+	 * @see #getType
+	 */
+	boolean isTypeMatch(String name, ResolvableType typeToMatch) throws NoSuchBeanDefinitionException;
+
+	/**
+	 * Check whether the bean with the given name matches the specified type.
+	 * More specifically, check whether a {@link #getBean} call for the given name
+	 * would return an object that is assignable to the specified target type.
+	 * <p>Translates aliases back to the corresponding canonical bean name.
+	 * Will ask the parent factory if the bean cannot be found in this factory instance.
+	 * @param name the name of the bean to query
+	 * @param typeToMatch the type to match against (as a {@code Class})
+	 * @return {@code true} if the bean type matches,
+	 * {@code false} if it doesn't match or cannot be determined yet
+	 * @throws NoSuchBeanDefinitionException if there is no bean with the given name
+	 * @since 2.0.1
+	 * @see #getBean
+	 * @see #getType
+	 */
+	boolean isTypeMatch(String name, Class<?> typeToMatch) throws NoSuchBeanDefinitionException;
+
+	/**
+	 * Determine the type of the bean with the given name. More specifically,
+	 * determine the type of object that {@link #getBean} would return for the given name.
+	 * <p>For a {@link FactoryBean}, return the type of object that the FactoryBean creates,
+	 * as exposed by {@link FactoryBean#getObjectType()}.
+	 * <p>Translates aliases back to the corresponding canonical bean name.
+	 * Will ask the parent factory if the bean cannot be found in this factory instance.
+	 * @param name the name of the bean to query
+	 * @return the type of the bean, or {@code null} if not determinable
+	 * @throws NoSuchBeanDefinitionException if there is no bean with the given name
+	 * @since 1.1.2
+	 * @see #getBean
+	 * @see #isTypeMatch
+	 */
+	Class<?> getType(String name) throws NoSuchBeanDefinitionException;
+
+	/**
+	 * Return the aliases for the given bean name, if any.
+	 * All of those aliases point to the same bean when used in a {@link #getBean} call.
+	 * <p>If the given name is an alias, the corresponding original bean name
+	 * and other aliases (if any) will be returned, with the original bean name
+	 * being the first element in the array.
+	 * <p>Will ask the parent factory if the bean cannot be found in this factory instance.
+	 * @param name the bean name to check for aliases
+	 * @return the aliases, or an empty array if none
+	 * @see #getBean
+	 */
+	String[] getAliases(String name);
+}
